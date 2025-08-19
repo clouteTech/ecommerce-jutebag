8926a1a1c093b9c4b0e9ae73e77ee8d7a64ef7f7
Keerthu10 <keerthanagurumoorthy@gmail.com>
2025-08-20 00:52:37 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ecommerce_Demo/",
+  base: "/ecommerce-demo",
   server: {
     host: true, 
     port: 5174,
