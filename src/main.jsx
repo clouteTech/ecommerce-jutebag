7cc92ad9bd05682ac33ce6ab40aa5f3de06c3833
Keerthu10 <keerthanagurumoorthy@gmail.com>
2025-08-20 13:25:01 +0530

    Changes json

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
@@ -10,7 +11,7 @@ import { CartProvider } from "./Context/CartContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/ecommerce-jutebag">
         <App />
       </BrowserRouter>
     </CartProvider>
