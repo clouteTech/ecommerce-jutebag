339a73051fb7b32136ab0419b91a70d724ddcf27
Keerthu10 <keerthanagurumoorthy@gmail.com>
2025-08-20 13:33:13 +0530

    vite network

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ecommerce-jutebag/",
-  // server: {
-  //   host: true,
-  //   port: 5174,
-  // },
+  base: "/ecommerce-jutebag/"
 });
